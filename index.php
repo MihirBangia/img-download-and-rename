a565bf37af63e0d22d6b8d31b087908ae3908212
Mihir Bangia <89865393+MihirBangia@users.noreply.github.com>
2023-10-03 18:58:39 +0530

    changes

--- index.php
+++ index.php
@@ -1,59 +1,73 @@
 <?php
 
-$csvFile = "InventoryDetails.csv";
+$csvFile = "image.csv";
 $csv = fopen($csvFile, "r");
-$destination = './image/';
+$destination = './product/';
 
 $sku = array();
 $newCsvData = array();
-// $data = array();
+$fileName = array();
+
 
 if ($csv !== FALSE) {
     $i = 0;
     while (!feof($csv)) {
         $data = fgetcsv($csv);
+
+
         if (!empty($data) && $i != 0) {
             array_push($sku, $data[0]);
+            array_push($fileName, $destination . basename($data[1]));
+
+
+            $size = getimagesize($data[1]);
+            $extension = explode('/', $size['mime'])[1];
+            // print_r($extension);
+            // echo "<br/>";
+
+
+            if ($extension == '') {
+                echo "extension not found";
+                continue;
+            }
             file_put_contents(
                 $destination . basename($data[1]),
                 file_get_contents($data[1])
             );
+
         }
         $i++;
         array_push($newCsvData, $data);
-        // print_r($data);
+
     }
     array_push($newCsvData[0], 'New Url');
 
-    $fileName = glob($destination . "*?", GLOB_BRACE);
 
-    for ($index = 0; $index < count($fileName); $index++) {
 
-        $size = getimagesize($fileName[$index]);
-        $extension = explode('/', $size['mime'])[1];
+    // for ($index = 0; $index < count($fileName); $index++) {
 
-        if ($extension == '') {
-            $rename = "NULL";
-            rename($fileName[$index], $destination . $rename);
-        } else {
-            $rename = $sku[$index] . "." . $extension;
-            rename($fileName[$index], $destination . $rename);
-        }
+    //     $size = getimagesize($fileName[$index]);
+    //     $extension = explode('/', $size['mime'])[1];
 
-        array_push($newCsvData[$index + 1], $rename);
-        unset($rename, $extension);
-    }
+    //     $rename = uniqid($index) . '.' . $extension;
+    //     rename($fileName[$index], $destination . $rename);
+
+    //     array_push($newCsvData[$index + 1], $rename);
+    //     unset($rename, $extension);
+    // }
 }
-print_r($newCsvData[42]);
+// print_r($fileName);
+// echo "<br/> ";
 fclose($csv);
 
-$updatedCsv = fopen($csvFile, 'w');
 
-if (!$updatedCsv)
-    die("Something went wrong!");
+// $updatedCsv = fopen('finalProductImage.csv', 'w');
+
+// if (!$updatedCsv)
+//     die("Something went wrong!");
 // fseek($updatedCsv, 0);
-foreach ($newCsvData as $coulmns) {
-    fputcsv($updatedCsv, $coulmns);
-}
-fclose($updatedCsv);
+// foreach ($newCsvData as $coulmns) {
+//     fputcsv($updatedCsv, $coulmns);
+// }
+// fclose($updatedCsv);
 ?>
